0b22f2990f93a544d7941dd98f094bb61d4fc4fc
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-22 16:56:42 +0530

    update

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 
 const Date = () => {
     return (
-        <div className='w-48 mx-auto my-20'>
-            <input 
-                type="date" 
-                className='w-48 border rounded-lg p-2'
-            />
-        </div>
+        <>
+            <div className='text-center mt-20'>   
+                <h1 className="text-xl font-semibold">
+                    Seminar Hall booking
+                </h1>
+            </div>
+            <div className='w-48 mx-auto my-12'>
+                <input 
+                    type="date" 
+                    className='w-48 border rounded-lg p-2'
+                />
+            </div>
+        </>
     )
 }
 
--- src/components/Seats.jsx
+++ src/components/Seats.jsx
@@ -4,9 +4,9 @@ const Seats = () => {
     const arr = new Array(50).fill(null);
 
     return (
-        <div className='flex flex-wrap justify-center gap-4 mx-auto m-8 w-10/12'>
+        <div className='flex flex-wrap justify-center gap-4 mx-auto m-8 sm:w-10/12'>
             {arr.map((val, index) => (
-                <div key={index} className="text-white bg-gray-500 w-20 py-4 rounded-lg flex justify-center cursor-pointer">
+                <div key={index} className="text-white bg-green-600 w-20 py-4 rounded-lg flex justify-center cursor-pointer">
                     {index + 1}
                 </div>
             ))}
