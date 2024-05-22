739b06739e7b4df49f725d5d6f296054afad69d2
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-22 17:34:09 +0530

    modal functionality worked

---
+++ src/components/BookingForm.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { useState } from 'react'
+
+const BookingForm = ({ showForm, setShowForm }) => {
+
+    return (
+        <div className={"fixed inset-0 flex items-center justify-center"}>
+            <div className="relative p-4 w-full max-w-md max-h-full">
+                {/* <!-- Modal content --> */}
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    {/* <!-- Modal header --> */}
+                    <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                            Edit Task
+                        </h3>
+                        <button onClick={() => setShowForm(false)}  type="button" className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                            X
+                        </button>
+                    </div>
+                    {/* <!-- Modal body --> */}
+                    <div className="p-4 md:p-5">
+                        <form className="space-y-4" >
+                            <div>
+                                <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                                <input
+                                    type="text"
+                                    name="title"
+                                    id="title"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                                <input
+                                    type="text"
+                                    name="description"
+                                    id="description"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white mb-4"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="dueDate" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+                                <input
+                                    type="date"
+                                    name="dueDate"
+                                    id="dueDate"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white mb-4"
+                                    required
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="w-full text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                            >
+                                Update
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BookingForm
--- src/components/Seats.jsx
+++ src/components/Seats.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import BookingForm from "./BookingForm";
+import { useState } from "react";
 
 const Seats = () => {
     const arr = new Array(50).fill(null);
+    
+    const [showForm, setShowForm] = useState(false)
 
     return (
         <div className='flex flex-wrap justify-center gap-4 mx-auto m-8 sm:w-10/12'>
             {arr.map((val, index) => (
-                <div key={index} className="text-white bg-green-600 w-20 py-4 rounded-lg flex justify-center cursor-pointer">
+                <div key={index} onClick={() => setShowForm(true)} className="text-white bg-green-600 w-20 py-4 rounded-lg flex justify-center cursor-pointer">
                     {index + 1}
                 </div>
             ))}
+            { showForm && 
+                <BookingForm showForm={showForm} setShowForm={setShowForm} />
+            }
         </div>
     )
 }
