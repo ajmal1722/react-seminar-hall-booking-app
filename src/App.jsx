1d93f1cc1cd8806411b17e8a9a4b9e9b753b6e0d
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-23 17:54:16 +0530

    signup page create

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import Date from './components/Date'
 import Seats from './components/Seats'
 import Login from './components/Login'
+import SignUp from './components/SignUp'
 
 function App() {
 
   return (
     <>
+      <SignUp/>
       <Login/>
       <Date/>
       <Seats/>
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const SignUp = () => {
+    return (
+        <div className='flex items-center justify-center h-screen'>
+            <div className="border border-yellow-400 rounded-lg lg:w-1/4 md:w-1/2 w-full m-4 p-6 text-center">
+                <h1 className='text-2xl font-semibold mb-8 mt-2'>Sign Up</h1>
+                <form action="">
+                    <input 
+                        name='name'
+                        id='name'
+                        type="text" 
+                        className='px-4 py-2 border border-gray-300 rounded-lg w-full mb-4 '
+                        placeholder='Your name...'
+                    />
+                    <input 
+                        name='email'
+                        id='email'
+                        type="text" 
+                        className='px-4 py-2 border border-gray-300 rounded-lg w-full mb-4 '
+                        placeholder='Your email...'
+                    />
+                    <input 
+                        name='password'
+                        id='password'
+                        type="password" 
+                        className='px-4 py-2 border border-gray-300 rounded-lg w-full mb-2'
+                        placeholder='Create a password...'
+                    />
+                    <p className='text-sm mb-6'>
+                        If you already have an account, click here to 
+                        <button type='button' className='px-1 text-blue-700 font-semibold hover:text-blue-900'>
+                            Login
+                        </button>
+                    </p>
+                    <button type='submit' className="bg-green-500 text-white font-semibold w-28 py-2 cursor-pointer rounded-xl hover:bg-green-600">
+                        Sign Up
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default SignUp
