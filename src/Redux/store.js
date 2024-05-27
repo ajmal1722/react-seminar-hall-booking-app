9f6011d8669896852c6b28aaf0f42bcaf58132f1
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-27 18:56:50 +0530

    redux-persist setup completed

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
-import selectedSeatReducer from './seats/seatSlice'
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import selectedSeatReducer from './seats/seatSlice';
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default configureStore({
-    reducer: {
-        selectSeat: selectedSeatReducer
-    }
-})
+// Persist configuration
+const persistConfig = {
+    key: 'root', // 'root' is the default key, you can use any key you want
+    storage,
+};
+
+// Root reducer
+const rootReducer = combineReducers({
+    selectSeat: selectedSeatReducer,
+});
+
+// Persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure store with persisted reducer
+const store = configureStore({
+    reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
